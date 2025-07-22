d91112eda94926b73190730ec9cb59941f51b5e3
Maaz Shaikh <maaz.z.shaikh@gmail.com>
2025-07-22 16:55:29 +0530

    final

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,19 +1,38 @@
 "use client";
 
-import React, { use, useState } from "react";
-import MeetingRoom from "@/components/MeetingRoom";
-import MeetingSetup from "@/components/MeetingSetup";
-import { useGetCallById } from "@/hooks/getCallById";
+import { useState } from "react";
 import { useUser } from "@clerk/nextjs";
 import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import { useParams } from "next/navigation";
 import { Loader } from "lucide-react";
-const Meeting = (props: { params: Promise<{ id: string }> }) => {
-  const { id } = use(props.params); // Unwraps the params Promise
-  const { user, isLoaded } = useUser();
-  const [isSetupComplete, setIsSetupComplete] = useState(false);
+
+import { useGetCallById } from "@/hooks/useGetCallById";
+import MeetingSetup from "@/components/MeetingSetup";
+import MeetingRoom from "@/components/MeetingRoom";
+import { Alert } from "@/components/ui/alert";
+
+const MeetingPage = () => {
+  const { id } = useParams();
+  const { isLoaded, user } = useUser();
   const { call, isCallLoading } = useGetCallById(id);
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
 
   if (!isLoaded || isCallLoading) return <Loader />;
+
+  if (!call)
+    return (
+      <p className="text-center text-3xl font-bold text-white">
+        Call Not Found
+      </p>
+    );
+
+  // get more info about custom call type:  https://getstream.io/video/docs/react/guides/configuring-call-types/
+  const notAllowed =
+    call.type === "invited" &&
+    (!user || !call.state.members.find((m) => m.user.id === user.id));
+
+  if (notAllowed)
+    return <Alert title="You are not allowed to join this meeting" />;
 
   return (
     <main className="h-screen w-full">
@@ -30,4 +49,4 @@ const Meeting = (props: { params: Promise<{ id: string }> }) => {
   );
 };
 
-export default Meeting;
+export default MeetingPage;
--- constants/index.ts
+++ constants/index.ts
@@ -25,5 +25,11 @@ const sidebarLinks = [
     imgUrl: "/icons/add-personal.svg",
   },
 ];
-
+export const avatarImages = [
+  "/images/avatar-1.jpeg",
+  "/images/avatar-2.jpeg",
+  "/images/avatar-3.png",
+  "/images/avatar-4.png",
+  "/images/avatar-5.png",
+];
 export { sidebarLinks };
